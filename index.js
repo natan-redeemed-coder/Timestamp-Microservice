31618cdeb89b63d3f10fc8b7c84d905d816142b9
Natan <natan.redeemed.coder@gmail.com>
2026-06-22 16:32:41 +0000

    Pending changes exported from your codespace

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+const cors = require("cors");
+const express = require("express");
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+const app = express();
 
 
+app.use(cors({optionsSuccessStatus: 200}));
 
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.use(express.static("public"));
+
+app.get(
+  "/",
+  (request, response) => {
+    response.sendFile(__dirname + "/views/index.html");
+  }
+);
+
+function isPositiveIntegerOrZero(string) {
+  return /^\d+$/.test(string);
+}
+
+app.get(
+  "/api/:date?",
+  (request, response) => {
+    if (request.params.date) {
+      const date = new Date(request.params.date);
+      if (isNaN(date.getTime())) {
+        const unixTime = Number(request.params.date)
+        if (isPositiveIntegerOrZero(unixTime)) {
+          response.json({
+            unix: unixTime,
+            utc: new Date(unixTime).toUTCString()
+          });
+        } else {
+          response.json({
+            error: "Invalid Date"
+          });
+        }
+      } else {
+        response.json({
+          unix: Math.floor(date.getTime()),
+          utc: date.toUTCString()
+        });
+      }
+    } else {
+      response.json({
+        unix: Math.floor(Date.now()),
+        utc: new Date().toUTCString()
+      });
+    }
+  }
+);
+
+var listener = app.listen(
+  process.env.PORT || 3000,
+  () => {
+    console.log("Your app is listening on port " + listener.address().port);
+  }
+);
